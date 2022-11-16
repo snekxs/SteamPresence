9add8d4b49632911d8f08f26ab8d5a8525df4002
Snekxs <snekxs@gmail.com>
2022-11-16 12:45:49 -0700

    Add .env Support

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
 const SteamUser = require("steam-user");
+const webhook = require("webhook-discord");
+require('dotenv').config()
+
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+
 
 steamClient = new SteamUser();
-
+const Hook = new webhook.Webhook(
+  "https://discord.com/api/webhooks/1041435170693197874/YR0ytItKLkPuRUkKwaczKJFI7edCETPdAx6RhhjSZNmY7OE3QM15IdwnBGyL9_QcFRPR"
+);
 steamClient.logOn({
-  accountName: "Snekxs",
-  password: "@Unit5006",
+  accountName: username,
+  password: password,
   rememberPassword: true,
   rememberMachine: true,
 });
+
 steamClient.on("loggedOn", () => {
   console.log("Logged into Steam");
 
   const userID = steamClient.steamID.getSteamID64();
-
+  Hook.success("Steam", `Logged into Steam as ${userID}`);
   console.log(`Logged in as (${userID})`);
-});
-steamClient.on("error", (err) => {
-  console.log(err);
+  return userID;
 });
